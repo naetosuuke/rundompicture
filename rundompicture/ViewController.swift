0b1c935d146d5e28e9fae14acfc5fa1539f3c2bc
naetosuuke <118874380+naetosuuke@users.noreply.github.com>
2022-11-28 21:30:22 +0900

    Just for uploading to GitHub

--- rundompicture/ViewController.swift
+++ rundompicture/ViewController.swift
@@ -3,7 +3,8 @@
 //  rundompicture
 //
 //  Created by Daisuke Doi on 2022/10/02.
-//
+//  This code is quoted from "たった2日でマスターできるiPhoneアプリ開発集中講座 Xcode 11　Swift 5対応 Kindle版" for my personal training. Not for distribution  https://www.amazon.co.jp/%E3%81%9F%E3%81%A3%E3%81%9F2%E6%97%A5%E3%81%A7%E3%83%9E%E3%82%B9%E3%82%BF%E3%83%BC%E3%81%A7%E3%81%8D%E3%82%8BiPhone%E3%82%A2%E3%83%97%E3%83%AA%E9%96%8B%E7%99%BA%E9%9B%86%E4%B8%AD%E8%AC%9B%E5%BA%A7-Xcode-11-Swift-5%E5%AF%BE%E5%BF%9C-%E8%97%A4%E6%B2%BB%E4%BB%81-ebook/dp/B081FG9J2V/ref=sr_1_3?keywords=%E3%81%9F%E3%81%A3%E3%81%9F2%E6%97%A5%E3%81%A7%E3%83%9E%E3%82%B9%E3%82%BF%E3%83%BC%E3%81%A7%E3%81%8D%E3%82%8Biphone%E3%82%A2%E3%83%97%E3%83%AA%E9%96%8B%E7%99%BA%E9%9B%86%E4%B8%AD%E8%AC%9B%E5%BA%A7&qid=1669637309&qu=eyJxc2MiOiIyLjYwIiwicXNhIjoiMi4xOCIsInFzcCI6IjIuMTIifQ%3D%3D&sprefix=%E3%81%9F%E3%81%A3%E3%81%9F2%E6%97%A5%2Caps%2C184&sr=8-3
+
 
 //アプリが用意しているライブラリUIKit を呼び出している　下のうす紫の字で宣言しているものがライブラリのコマンド
 import UIKit
